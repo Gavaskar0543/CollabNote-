cd1e1665c727c08716fa034d6d9f645d54cbda90
Gavaskar <gavaskaraeg@gmail.com>
2023-11-27 04:19:23 +0000

    CROS in Node. js is a mechanism

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const port = 8000;
 const server = express();
+const cors = require('cors');
 
-
+server.use(cors());
+server.get('/',(req,res)=>{
+    res.status(200).json({
+       message: "Successful Establishment of Connection with Backend Server",
+       success:true
+    })
+})
 server.listen(port,function(err){
     if(err){
         console.log(err.message);
